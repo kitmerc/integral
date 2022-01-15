d3578859eedc51988f2d871d5322142faf53e953
kitmerc <97457279+kitmerc@users.noreply.github.com>
2022-01-15 23:41:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,24 +16,10 @@ int main()
     n = (b - a) / h;
 
     sintegral = h * ((cos(a/2) + cos(b/2)) / 2);
-    cout << sintegral;
     for( int i = 1; i <= n-1; i++)
     {
         sintegral = sintegral + h * cos((a + h * i)/2);
         cout << sintegral << "\r\n";
     }
     cout << "integral = " << sintegral <<"\n";
-    m =  2 * sin(1/2);
-    cout << m << "\r\n";
-/***
-check
-***/
-
-    float rez;
-
-    rez = (2 * sin(b/2)) - (2 * sin(a/2));
-
-    cout << "integral check =  " << rez;
-
-    return 0;
 }
